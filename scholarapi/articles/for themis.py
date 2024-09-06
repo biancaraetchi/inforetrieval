5db439dd49464deb450daaaeedd930c887863df5
biancaraetchi <mold.academ@gmail.com>
2024-09-06 19:33:55 +0200

    Changed fetching for total citations + added file for checking themis

--- scholarapi/articles/for themis.py
+++ scholarapi/articles/for themis.py
@@ -1,13 +1,15 @@
 import requests
 import csv, re
-import os
-from django.http import JsonResponse, HttpResponse
-from dotenv import load_dotenv
+# from django.http import JsonResponse, HttpResponse
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-base_url = os.getenv('BASE_URL')
-api_key = os.getenv('API_KEY')
+# base_url = os.getenv('BASE_URL')
+# api_key = os.getenv('API_KEY')
+
+base_url = "https://serpapi.com/"
+api_key = "insert api key here"
 
 # Endpoint to fetch author ids for a particular name
 # Should change the API to the Profiles API
@@ -28,45 +30,56 @@ def fetch_author_id(author_names):
 
 # Endpoint to fetch + csv the data
 # Using a predefined author id for now
-def fetch_articles(request):
+def fetch_articles(authors):
     # options for sort: either blank or 'pubdate'
-    sort = request.GET.get('sort', "")
-    authors = request.GET.get('authors', "")
+    # sort = request.GET.get('sort', "")
+    # authors = request.GET.get('authors', "")
     if authors:
-        author_ids, citations_new = fetch_author_id(authors)
+        author_ids, citations = fetch_author_id(authors)
         if not author_ids:
-            return JsonResponse({})
+            # return JsonResponse({})
+            return []
         articles = []
         for id in author_ids:
-            api_url = base_url+"search?engine=google_scholar_author&author_id="+id+"&api_key="+api_key+"&sort="+sort
+            api_url = base_url+"search?engine=google_scholar_author&author_id="+id+"&api_key="+api_key
+            # +"&sort="+sort
             try:
                 response = requests.get(api_url)
                 json_data = response.json()
                 articles.append(json_data["articles"])
             
             except requests.RequestException as e:
-                return JsonResponse({'error': 'Failed to fetch data', 'details': str(e)}, status=500)
+                return []
+                # return JsonResponse({'error': 'Failed to fetch data', 'details': str(e)}, status=500)
 
             # Create a HttpResponse object with CSV header
-            response = HttpResponse(content_type='text/csv')
-            response['Content-Disposition'] = 'attachment; filename="data.csv"'
+            # response = HttpResponse(content_type='text/csv')
+            # response['Content-Disposition'] = 'attachment; filename="data.csv"'
 
             # Create a CSV writer object
-            writer = csv.writer(response)
+            # writer = csv.writer(response)
 
             articles = json_data["articles"]
-            writer.writerow(['Title','Authors','Year','Citations'])
+            # writer.writerow(['Title','Authors','Year','Citations'])
+            print('Title,Authors,Year,Citations')
 
             # Write data rows
             for article in articles:
                 citation = article["cited_by"].get("value")
                 if citation is None:
                     citation = 0
-                writer.writerow([article["title"], article["authors"],article["year"],str(citation)])
+                # writer.writerow([article["title"], article["authors"],article["year"],str(citation)])
+                print(article["title"]+";"+article["authors"]+";"+article["year"]+";"+str(citation))
+
             
             # writer.writerow(["Total citations;",str(citations)])
-            writer.writerow(["Total citations;",str(citations_new)])
             # For Themis
-            # print(response)
-            return response
-    return JsonResponse({'error': 'No authors provided'}, status=400)
+            print("Total citations;",str(citations))
+            # return response
+    # return JsonResponse({'error': 'No authors provided'}, status=400)
+    return []
+
+input1 = input()
+input2 = input()
+input3 = input()
+fetch_articles(input2)
